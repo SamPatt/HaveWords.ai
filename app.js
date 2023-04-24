d35c0cc9f78a1facd4d730b164386d1564b88cd7
Steve Dekorte <steve@dekorte.com>
2023-04-24 14:53:29 -0700

    added mic icon to players chat column header
    Not sure if mute is working yet.
    Also added svg icons for mic in resources folder.

--- app.js
+++ app.js
@@ -75,13 +75,90 @@ const startGameButton = document.getElementById('startGameButton');
 let guestUserList = [];
 let userAudioStream;
 
-navigator.mediaDevices.getUserMedia({ audio: true })
-  .then(stream => {
-    userAudioStream = stream;
-  })
-  .catch(error => {
-    console.error('Error getting audio stream:', error);
-  });
+/* --- microphone input ---*/
+
+const micButton = document.getElementById('micButton');
+micButton._isOn = false;
+micButton._onIcon = "mic-fill-svgrepo-com.svg";
+micButton._offIcon = "mic-slash-fill-svgrepo-com.svg";
+
+micButton.toggleState = function () {
+  if (this._isOn) {
+    this.turnOff()
+  } else {
+    this.turnOn()
+  }
+}
+
+micButton.setIsOn = function (bool) {
+  this._isOn = bool
+  this.updateIcon()
+}
+
+micButton.svgObject = function () {
+  return document.getElementById("micSvgIcon")
+}
+
+micButton.updateIcon = function () {
+  const icon = this._isOn ? this._onIcon : this._offIcon;
+  this.svgObject().setAttribute("data", "resources/icons/" + icon)
+}
+
+micButton.turnOn = function () {
+  this.setIsOn(true)
+}
+
+micButton.turnOff = function () {
+  this.setIsOn(false)
+}
+
+micButton.addEventListener('click', (event) => {
+  const self = event.target
+  //this.toggleState()
+  if (self._isOn) {
+    if (hasMicAccess()) {
+        userAudioStream.muted = true // not sure if this works
+    }
+    self.setIsOn(false)
+  } else {
+    if (hasMicAccess()) {
+      userAudioStream.muted = false // not sure if this works
+      //userAudioStream = undefined;
+      self.setIsOn(true)
+    } else {
+      requestMicAccess()
+    }
+  }
+});
+
+function hasMicAccess () {
+  return (userAudioStream !== undefined);
+}
+
+let isRequestingMicAccess = false
+
+function requestMicAccess () {
+  if (hasMicAccess() || isRequestingMicAccess) {
+    return 
+  }
+
+  isRequestingMicAccess = true
+
+  navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+      isRequestingMicAccess = false
+      userAudioStream = stream;
+      micButton.setIsOn(true)
+    })
+    .catch(error => {
+      isRequestingMicAccess = false
+      micButton.setIsOn(false)
+      console.error('Error getting audio stream:', error);
+    }
+  );
+}
+
+/* --------------------- */
 
 peer.on('open', function () {
     console.log('PeerJS client is ready. Peer ID:', id);
