a58d331c340ad51d3573148e3ee7517e6bb09eb8
Steve Dekorte <steve@dekorte.com>
2023-04-25 21:43:05 -0700

    Sounds.js cleanup, removing old Microphone code from app.js

--- source/app/Sounds.js
+++ source/app/Sounds.js
@@ -14,9 +14,13 @@
     super.init();
   }
 
+  audioContext () {
+    const context = new (window.AudioContext || window.webkitAudioContext)();
+    return content
+  }
+
   playOminousSound () {
-    const audioContext = new (window.AudioContext ||
-      window.webkitAudioContext)();
+    const audioContext = this.audioContext()
 
     const notes = [110, 123.47, 130.81, 146.83]; // Frequencies for notes A2, B2, C3, and D3
 
@@ -38,14 +42,12 @@
     });
   }
 
-  playSendBeep () {
-    const context = new (window.AudioContext || window.webkitAudioContext)();
+  playNotes (notes) {
+    const context = this.audioContext()
     const gainNode = context.createGain();
 
     gainNode.gain.setValueAtTime(0.02, context.currentTime);
     gainNode.connect(context.destination);
-
-    const notes = [330, 290]; // Lower frequencies for the send beep
 
     notes.forEach((note, index) => {
       const oscillator = context.createOscillator();
@@ -61,27 +63,12 @@
     });
   }
 
+  playSendBeep () {
+    this.playNotes([330, 290])
+  }
+
   playReceiveBeep () {
-    const context = new (window.AudioContext || window.webkitAudioContext)();
-    const gainNode = context.createGain();
-
-    gainNode.gain.setValueAtTime(0.02, context.currentTime);
-    gainNode.connect(context.destination);
-
-    const notes = [290, 330]; // Lower frequencies for the receive beep
-
-    notes.forEach((note, index) => {
-      const oscillator = context.createOscillator();
-      oscillator.type = "sine";
-      oscillator.frequency.setValueAtTime(
-        note,
-        context.currentTime + index * 0.1
-      );
-
-      oscillator.connect(gainNode);
-      oscillator.start(context.currentTime + index * 0.1);
-      oscillator.stop(context.currentTime + index * 0.1 + 0.1);
-    });
+    this.playNotes([290, 330]);
   }
   
 }.initThisClass());
